c48aaad6030629794d0cef485f480c0d4fb592c8
rial99 <ratulstephenite99@gmail.com>
2018-01-19 21:17:15 +0530

    final resolution for project reference

--- app.py
+++ app.py
@@ -196,6 +196,60 @@ def add_article():
 
     return render_template('add_article.html', form=form)
 
+#edit article
+@app.route('/edit_article/<string:id>',methods=['GET','POST'])
+@is_logged_in
+def edit_article(id):
+    #create cursor
+    cur = mysql.connection.cursor()
+    #get articles by id
+    result = cur.execute("SELECT * FROM articles where id = %s", [id])
+
+    article = cur.fetchone()
+    #get form
+    form = ArticleForm(request.form)
+    #populate article from fields
+    form.title.data = article['title']
+    form.body.data = article['body']
+
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        #create cursor
+        cur = mysql.connection.cursor()
+        #execute
+        cur.execute("UPDATE articles SET title=%s, body=%s WHERE id = %s", (title,body,[id]))
+
+        #commit to db
+        mysql.connection.commit()
+
+        #close connection
+        cur.close()
+
+        flash('Article updated','success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('edit_article.html', form=form)
+#delete article
+@app.route('/delete_article/<string:id>',methods=['POST'])
+@is_logged_in
+def delete_article(id):
+    #create cursor
+    cur = mysql.connection.cursor()
+    #execute
+    cur.execute("DELETE FROM articles WHERE id = %s",[id])
+    #commit to db
+    mysql.connection.commit()
+
+    #close connection
+    cur.close()
+    flash('Article Deleted','success')
+
+    return redirect(url_for('dashboard'))
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'my secret key'
